5842ba78d09e7016c8ae8f6f10d718d954f9d8a4
Ramiro S. Garay <ramiro.sgaray@gmail.com>
2022-04-15 09:48:50 -0300

    add another test and fix txInfoMint in previous one

--- onchain/test/SpecNFT.hs
+++ onchain/test/SpecNFT.hs
@@ -1,50 +1,77 @@
 {-# LANGUAGE OverloadedStrings #-}
-module SpecNFT where
+module SpecNFT(nftTests) where
 
-import Utils
-import Test.Tasty
-import Test.Tasty.HUnit
+import Utils ( succeeds, fails )
+import Test.Tasty ( testGroup, TestTree )
+import Test.Tasty.HUnit ( testCase )
 import Settings(bondedStakingTokenName)
 import NFT(pbondedStakingNFTPolicy)
 import Plutarch.Api.V1
+    ( PScriptContext, mintingPolicySymbol, mkMintingPolicy )
 import Plutus.V1.Ledger.Api
+    ( singleton,
+      CurrencySymbol,
+      Value,
+      Address(addressCredential, addressStakingCredential, Address),
+      Credential(PubKeyCredential),
+      PubKeyHash(PubKeyHash),
+      ScriptContext (ScriptContext, scriptContextTxInfo, scriptContextPurpose),
+      ScriptPurpose(Minting),
+      TxInInfo(TxInInfo, txInInfoResolved, txInInfoOutRef),
+      TxInfo(TxInfo, txInfoInputs, txInfoFee, txInfoDCert, txInfoWdrl,
+             txInfoValidRange, txInfoSignatories, txInfoData, txInfoId,
+             txInfoOutputs, txInfoMint),
+      TxOut(TxOut, txOutAddress, txOutValue, txOutDatumHash),
+      TxOutRef (txOutRefId, txOutRefIdx, TxOutRef),
+      TxId(TxId),
+      always )
 import Plutus.V1.Ledger.Ada(lovelaceValueOf)
 import Plutarch.Unsafe (punsafeCoerce)
 
 nftTests :: TestTree
 nftTests = testGroup
     "NFT tests"
-    [ testCase "should mint once and consume the outRef" $
+    [ testCase "should validate correct transaction" $
         succeeds $ testPolicy # pconstant () # pconstant goodCtx
     , testCase "should not mint more than once" $
-        fails $ testPolicy # pconstant () # pconstant badCtx1]
-        
--- Test data
+        fails $ testPolicy # pconstant () # pconstant badCtx1
+    , testCase "should not consume the wrong outRef" $
+        fails $ testPolicy # pconstant () # pconstant badCtx2]
+
+-- Test data --
+
+-- The CurrencySymbol associated with the policy
 testCurrencySymbol :: CurrencySymbol
 testCurrencySymbol =
     mintingPolicySymbol $ mkMintingPolicy $ punsafeCoerce $ testPolicy
         
+-- The policy
 testPolicy :: forall (s :: S) . Term s (PUnit :--> PScriptContext :--> PUnit)
-testPolicy = pbondedStakingNFTPolicy # pconstant testTxOutRef
+testPolicy = pbondedStakingNFTPolicy # pconstant testInputTxOutRef
 
-testTxOutRef :: TxOutRef
-testTxOutRef = TxOutRef {
+-- The UTXO used to mint the NFT
+testInputTxOutRef :: TxOutRef
+testInputTxOutRef = TxOutRef {
     txOutRefId = TxId "ffffeeee",
     txOutRefIdx = 0
 }
 
+-- The value contained by the previous UTXO
+testInputTxOut :: TxOut
+testInputTxOut = TxOut {
+    txOutAddress = testTxOutAddr,
+    txOutValue = lovelaceValueOf 50_000_000,
+    txOutDatumHash = Nothing
+}
+
+-- The address of all the the UTXOs involved (no validators are used)
 testTxOutAddr :: Address
 testTxOutAddr = Address {
     addressCredential = PubKeyCredential testPubKeyHash,
     addressStakingCredential = Nothing
 }
 
-testTxInAddr :: Address
-testTxInAddr = Address {
-    addressCredential = PubKeyCredential testPubKeyHash,
-    addressStakingCredential = Nothing
-}
-
+-- The public key hash used for signing
 testPubKeyHash :: PubKeyHash
 testPubKeyHash = PubKeyHash "deadbeef"
 
@@ -54,7 +81,7 @@ goodCtx = ScriptContext {
     scriptContextTxInfo = TxInfo {
         txInfoInputs = [
             TxInInfo {
-                txInInfoOutRef = testTxOutRef,
+                txInInfoOutRef = testInputTxOutRef,
                 txInInfoResolved = TxOut {
                     txOutAddress = testTxOutAddr,
                     txOutValue = lovelaceValueOf 50_000_000,
@@ -88,11 +115,27 @@ badCtx1 = goodCtx {
         txInfoOutputs = [
             TxOut {
                 txOutAddress = testTxOutAddr,
-                txOutValue = singleton
-                    testCurrencySymbol
-                    bondedStakingTokenName
-                    10,
+                txOutValue = badMint1,
                 txOutDatumHash = Nothing
+            }
+        ]
+        , txInfoMint = badMint1
+    }
+    , scriptContextPurpose = scriptContextPurpose goodCtx
+}
+
+-- The transaction does not consume the NFT's matching UTXO
+badCtx2 :: ScriptContext
+badCtx2 = goodCtx {
+    scriptContextTxInfo = (scriptContextTxInfo goodCtx) {
+        txInfoInputs = [
+            TxInInfo {
+                -- Wrong OutRef
+                txInInfoOutRef = TxOutRef {
+                    txOutRefId = TxId "dddddddd",
+                    txOutRefIdx = 0
+                },
+                txInInfoResolved = testInputTxOut
             }
         ]
     }
@@ -101,3 +144,6 @@ badCtx1 = goodCtx {
 
 goodMint :: Value
 goodMint = singleton testCurrencySymbol bondedStakingTokenName 1
+
+badMint1 :: Value
+badMint1 = singleton testCurrencySymbol bondedStakingTokenName 10
