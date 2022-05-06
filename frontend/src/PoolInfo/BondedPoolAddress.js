f4cd449670392cb7186e9565ad40eff5af710c50
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-05-06 12:26:23 +0700

    Remove static pool info

--- frontend/src/PoolInfo/BondedPoolAddress.js
+++
@@ -1,3 +0,0 @@
-exports._bondedPoolAddress = {
-    "address": "addr_test1wp6yyn9eraghjgmjxsact7jv3r6mv7jkz3ypqlep86vekvcrkah7j"
-}
--- frontend/src/PoolInfo/StateNFTSymbol.js
+++
@@ -1,3 +0,0 @@
-exports._stateNFTSymbol = {
-    "unCurrencySymbol": "1c0ccf01f7ce447ef153874b3b4b5756ea155afe3883be68e5a1f1e7"
-}
