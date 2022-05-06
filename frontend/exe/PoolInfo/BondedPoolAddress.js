851bd732d381fb27e793f9b5350f3a1448054016
rmgaray <ramiro.sgaray@gmail.com>
2022-05-05 21:55:50 -0300

    add information files with sample data

---
+++ frontend/exe/PoolInfo/BondedPoolAddress.js
@@ -0,0 +1,3 @@
+exports._bondedPoolAddress = {
+    "address": "addr_test1wp6yyn9eraghjgmjxsact7jv3r6mv7jkz3ypqlep86vekvcrkah7j"
+}
---
+++ frontend/exe/PoolInfo/ListNFTSymbol.js
@@ -0,0 +1,3 @@
+exports._listNFTSymbol = {
+    "unCurrencySymbol": "8c1f7568aa1f660f60388b73e7e47666127d927ba45a3f53e8471d76"
+}
