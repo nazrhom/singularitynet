69b7867db2ee4e80b3d2db0189dfeb392ca64f76
rmgaray <ramiro.sgaray@gmail.com>
2022-05-05 21:56:35 -0300

    call contracts from Main file

--- frontend/exe/Main.purs
+++ frontend/exe/Main.purs
@@ -2,14 +2,13 @@ module Main (main) where
 
 import Contract.Prelude
 
-import Contract.Address (ownPaymentPubKeyHash)
 import Contract.Monad (defaultContractConfig, runContract_)
+import CreatePool (createPoolContract)
+import DepositPool (depositPoolContract)
 import Effect.Aff (launchAff_)
 
 main :: Effect Unit
 main = launchAff_ $ do
   cfg <- defaultContractConfig
-  runContract_ cfg
-    $ log
-    <<< show
-    =<< ownPaymentPubKeyHash
+  runContract_ cfg createPoolContract
+  --runContract_ cfg depositPoolContract
