a16d39c730bb6605c20b05552f47749eb5a023bf
rmgaray <ramiro.sgaray@gmail.com>
2022-05-05 21:51:10 -0300

    add utility functions for Naturals and BigInts

--- frontend/exe/Utils.purs
+++ frontend/exe/Utils.purs
@@ -1,17 +1,14 @@
 module Utils(
     jsonReader
+    , nat
+    , big
 ) where
 
 import Contract.Prelude
-import Data.Argonaut(
-    Json
-  , class DecodeJson
-  , JsonDecodeError(TypeMismatch)
-  , caseJsonObject
-  , getField
-  )
 
-import Foreign.Object (Object)
+import Data.Argonaut (Json, class DecodeJson, JsonDecodeError(TypeMismatch), caseJsonObject, getField)
+import Contract.Numeric.Natural(Natural, fromBigInt')
+import Data.BigInt(BigInt, fromInt)
 
 -- | Helper to decode the local inputs such as unapplied minting policy and
 -- | typed validator
@@ -23,3 +20,10 @@ jsonReader
   -> Either JsonDecodeError a
 jsonReader field = caseJsonObject (Left $ TypeMismatch "Expected Object")
   $ flip getField field
+
+-- | Convert from `Int` to `Natural`
+nat :: Int -> Natural
+nat = fromBigInt' <<< fromInt
+-- | Convert from `Int` to `BigInt`
+big :: Int -> BigInt
+big = fromInt
