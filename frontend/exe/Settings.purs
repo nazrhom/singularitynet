aba9d568f9ef90d6d73713b6c3e30007c29d285e
rmgaray <ramiro.sgaray@gmail.com>
2022-05-05 21:57:11 -0300

    add settings file with token name and hardcoded pool parameters function

---
+++ frontend/exe/Settings.purs
@@ -0,0 +1,46 @@
+module Settings(
+    bondedStakingTokenName
+    , hardCodedParams
+    ) where
+
+import Prelude
+
+import Contract.Numeric.NatRatio (fromNaturals, toRational)
+import Contract.Numeric.Rational(Rational)
+import Contract.Value (CurrencySymbol, TokenName, adaSymbol, adaToken, mkTokenName)
+import Data.Maybe (Maybe)
+import Data.Tuple (Tuple(..))
+import Types (AssetClass(..), BondedPoolParams(..))
+import Types.ByteArray (byteArrayFromAscii)
+import Types.UnbalancedTransaction (PaymentPubKeyHash)
+import Utils (nat, big)
+
+bondedStakingTokenName :: Maybe TokenName
+bondedStakingTokenName = mkTokenName =<< byteArrayFromAscii "BondedStakingToken"
+
+
+interest' :: Maybe Rational
+interest' = toRational <$> fromNaturals (nat 1) (nat 100)
+
+-- Temporary, serialise to JSON
+hardCodedParams ::
+    PaymentPubKeyHash ->
+    CurrencySymbol ->
+    CurrencySymbol ->
+    Maybe BondedPoolParams
+hardCodedParams adminPkh nftCs assocListCs = do
+    interest <- interest'
+    pure $ BondedPoolParams {
+      iterations : nat 3
+      , start: big 1000
+      , end: big 2000
+      , userLength: big 100
+      , bondingLength: big 4
+      , interest
+      , minStake: nat 1000
+      , maxStake: nat 10_000
+      , admin: adminPkh
+      , bondedAssetClass: AssetClass $ Tuple adaSymbol adaToken
+      , nftCs
+      , assocListCs
+      }
