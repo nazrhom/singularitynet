466b20f80e6230ad4f4d7e2c7fe98dd38e272c7a
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-05-06 11:00:38 +0700

    Use explicit config

--- frontend/exe/Main.purs
+++ frontend/exe/Main.purs
@@ -2,13 +2,34 @@ module Main (main) where
 
 import Contract.Prelude
 
-import Contract.Monad (defaultContractConfig, runContract_)
+import Contract.Address ( NetworkId(TestnetId))
+import Contract.Monad
+  ( ConfigParams(ConfigParams)
+  , LogLevel(Trace)
+  , defaultDatumCacheWsConfig
+  , defaultOgmiosWsConfig
+  , defaultServerConfig
+  , defaultSlotConfig
+  , mkContractConfig
+  , runContract_
+  )
+import Contract.Wallet (mkNamiWalletAff)
 import CreatePool (createPoolContract)
 import DepositPool (depositPoolContract)
 import Effect.Aff (launchAff_)
 
 main :: Effect Unit
 main = launchAff_ $ do
-  cfg <- defaultContractConfig
+  wallet <- Just <$> mkNamiWalletAff
+  cfg <- mkContractConfig $ ConfigParams
+    { ogmiosConfig: defaultOgmiosWsConfig
+    , datumCacheConfig: defaultDatumCacheWsConfig
+    , ctlServerConfig: defaultServerConfig
+    , networkId: TestnetId
+    , slotConfig: defaultSlotConfig
+    , logLevel: Trace
+    , extraConfig: {}
+    , wallet
+    }
   runContract_ cfg createPoolContract
 --runContract_ cfg depositPoolContract
