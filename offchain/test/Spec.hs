04bd8db63b2fa59536ba71e02d369669141b0235
rmgaray <ramiro.sgaray@gmail.com>
2022-05-14 13:49:51 -0300

    remove redundant imports

--- offchain/test/Spec.hs
+++ offchain/test/Spec.hs
@@ -1,21 +1,15 @@
 module Main (main) where
 
-import Cardano.Prelude (Text)
-import Plutus.Contract (Contract)
-import Plutus.PAB.Effects.Contract.Builtin (EmptySchema)
-import Test.Plutip.Contract (initAda, assertExecution, withContract)
-import Test.Plutip.Predicate(shouldSucceed)
-import Test.Plutip.LocalCluster (withCluster)
-import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty (defaultMain, testGroup)
 
-import SingularityNetOffchain(loadPlutusScript)
-import SpecStateNFT(specStateNFT)
+import SingularityNetOffchain (loadPlutusScript)
+import SpecStateNFT (specStateNFT)
 
 main :: IO ()
 main = do
   Just statePolicyScript <- loadPlutusScript "BondedStateNFT"
-  defaultMain $ testGroup
-    "SingularityNet off-chain tests"
-    [
-      specStateNFT statePolicyScript 
-    ]
+  defaultMain $
+    testGroup
+      "SingularityNet off-chain tests"
+      [ specStateNFT statePolicyScript
+      ]
