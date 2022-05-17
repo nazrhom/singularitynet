f3d5f7101a0a65330ee81d7e857e6290a6b2d243
Michael Hollister <michael.hollister@mlabs.city>
2022-05-17 10:28:26 -0500

    Defaulting to Bonded tests

--- frontend/exe/Main.purs
+++ frontend/exe/Main.purs
@@ -12,7 +12,7 @@ import Contract.Monad
   , defaultServerConfig
   , defaultSlotConfig
   , launchAff_
-  , liftContractM
+  -- , liftContractM
   , mkContractConfig
   , runContract_
   )
@@ -22,8 +22,9 @@ import Data.Int (toNumber)
 import DepositPool (depositPoolContract)
 import Effect.Aff (delay)
 import Effect.Aff.Class (liftAff)
-import Settings (testInitUnbondedParams)
-import UnbondedStaking.CreatePool (createUnbondedPoolContract)
+
+-- import Settings (testInitUnbondedParams)
+-- import UnbondedStaking.CreatePool (createUnbondedPoolContract)
 
 main :: Effect Unit
 main = launchAff_ $ do
@@ -40,14 +41,14 @@ main = launchAff_ $ do
     }
   runContract_ cfg $ do
     -- Bonded test
-    -- poolInfo <- createBondedPoolContract
-    -- -- sleep in order to wait for tx
-    -- liftAff $ delay $ wrap $ toNumber 80_000
-    -- depositPoolContract poolInfo
-    -- liftAff $ delay $ wrap $ toNumber 80_000
-    -- closePoolContract poolInfo
+    poolInfo <- createBondedPoolContract
+    -- sleep in order to wait for tx
+    liftAff $ delay $ wrap $ toNumber 80_000
+    depositPoolContract poolInfo
+    liftAff $ delay $ wrap $ toNumber 80_000
+    closePoolContract poolInfo
 
-    -- Unbonded test
-    initParams <- liftContractM "main: Cannot initiate unbonded parameters"
-      testInitUnbondedParams
-    void $ createUnbondedPoolContract initParams
+-- Unbonded test
+-- initParams <- liftContractM "main: Cannot initiate unbonded parameters"
+--   testInitUnbondedParams
+-- void $ createUnbondedPoolContract initParams
