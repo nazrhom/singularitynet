eb7b11ab52e9d4de969ca80aaf6ec2696bfe7bb7
rmgaray <ramiro.sgaray@gmail.com>
2022-06-11 03:23:35 -0300

    update Spec.hs and comment out specPoolCreate while fixing user stake

--- offchain/test/Spec.hs
+++ offchain/test/Spec.hs
@@ -3,13 +3,19 @@ module Main (main) where
 import Test.Tasty (defaultMain, testGroup)
 
 import SingularityNetOffchain (loadPlutusScript)
-import SpecStateNFT (specStateNFT)
+import SpecUserStake (specUserStake)
+--import SpecPoolCreate (specPoolCreate)
+import Types (BondedPoolScripts(BondedPoolScripts, statePolicyScript, listPolicyScript, validatorScript))
 
 main :: IO ()
 main = do
   Just statePolicyScript <- loadPlutusScript "BondedStateNFT"
+  Just listPolicyScript <- loadPlutusScript "BondedListNFT"
+  Just validatorScript <- loadPlutusScript "BondedPoolValidator"
+  let bondedPoolScripts = BondedPoolScripts { statePolicyScript, listPolicyScript, validatorScript }
   defaultMain $
     testGroup
       "SingularityNet off-chain tests"
-      [ specStateNFT statePolicyScript
+      [ -- specPoolCreate bondedPoolScripts
+       specUserStake bondedPoolScripts
       ]
