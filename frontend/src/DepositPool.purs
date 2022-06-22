9e679d9004f7b52a4e82c72787ddbfcd2cca75a8
Michael Hollister <michael.hollister@mlabs.city>
2022-06-22 17:14:33 -0500

    Moved back deposit functions into module

--- frontend/src/DepositPool.purs
+++ frontend/src/DepositPool.purs
@@ -2,21 +2,27 @@ module DepositPool (depositBondedPoolContract) where
 
 import Contract.Prelude
 
-import AdminUtils (mkEntryUpdateList)
 import Contract.Address (AddressWithNetworkTag(AddressWithNetworkTag), getNetworkId, getWalletAddress, ownPaymentPubKeyHash, scriptHashAddress)
 import Contract.Monad (Contract, liftContractM, liftedE', liftedM, logInfo', throwContractError)
-import Contract.PlutusData (PlutusData, fromData, getDatumByHash)
+import Contract.Numeric.Rational (Rational, (%))
+import Contract.PlutusData (Datum(Datum), PlutusData, fromData, getDatumByHash, toData)
+import Contract.Prim.ByteArray (ByteArray)
 import Contract.ScriptLookups as ScriptLookups
 import Contract.Scripts (validatorHash)
-import Contract.TxConstraints (TxConstraints, mustBeSignedBy)
+import Contract.Transaction (TransactionInput, TransactionOutput)
+import Contract.TxConstraints (TxConstraints, mustBeSignedBy, mustPayToScript, mustSpendScriptOutput)
 import Contract.Utxos (utxosAt)
+import Contract.Value (mkTokenName, singleton)
 import Control.Applicative (unless)
+import Data.BigInt (BigInt)
 import Plutus.FromPlutusType (fromPlutusType)
 import Scripts.PoolValidator (mkBondedPoolValidator)
 import Settings (bondedStakingTokenName)
-import Types (BondedStakingDatum(StateDatum), BondedPoolParams(BondedPoolParams))
+import Types (BondedPoolParams(BondedPoolParams), BondedStakingAction(AdminAct), BondedStakingDatum(AssetDatum, EntryDatum, StateDatum), Entry(Entry))
 import Types.Natural (Natural)
-import Utils (getUtxoWithNFT, logInfo_, mkOnchainAssocList, splitByLength, submitTransaction, toIntUnsafe)
+import Types.Redeemer (Redeemer(Redeemer))
+import Types.Scripts (ValidatorHash)
+import Utils (getUtxoWithNFT, logInfo_, mkOnchainAssocList, mkRatUnsafe, roundUp, splitByLength, submitTransaction, toIntUnsafe)
 
 -- Deposits a certain amount in the pool
 depositBondedPoolContract ::
@@ -118,7 +124,7 @@ depositBondedPoolContract
         else
           let updateBatches = splitByLength (toIntUnsafe batchSize) updateList
           in mconcat <$> for updateBatches \txBatch -> do
-            failedDeposits' <- submitTransaction constraints lookups txBatch 
+            failedDeposits' <- submitTransaction constraints lookups txBatch
             batchFinishCallback failedDeposits'
             pure failedDeposits'
       logInfo_
@@ -133,3 +139,93 @@ depositBondedPoolContract
         \rewards for"
     _ ->
       throwContractError "depositBondedPoolContract: Datum incorrect type"
+
+-- | Creates a constraint and lookups list for updating each user entry
+mkEntryUpdateList
+  :: BondedPoolParams
+  -> ValidatorHash
+  -> (ByteArray /\ TransactionInput /\ TransactionOutput)
+  -> Contract ()
+       ( Tuple (TxConstraints Unit Unit)
+           (ScriptLookups.ScriptLookups PlutusData)
+       )
+mkEntryUpdateList
+  (BondedPoolParams { interest, bondedAssetClass, assocListCs })
+  valHash
+  (_ /\ txIn /\ txOut) = do
+  -- Get the Entry datum of the old assoc. list element
+  dHash <- liftContractM
+    "mkEntryUpdateList: Could not get Entry Datum Hash"
+    (unwrap txOut).dataHash
+  logInfo_ "mkEntryUpdateList: datum hash" dHash
+  listDatum <-
+    liftedM
+      "mkEntryUpdateList: Cannot get Entry's datum" $ getDatumByHash dHash
+  bondedListDatum :: BondedStakingDatum <-
+    liftContractM
+      "mkEntryUpdateList: Cannot extract NFT State datum"
+      $ fromData (unwrap listDatum)
+  -- The get the entry datum
+  case bondedListDatum of
+    EntryDatum { entry } -> do
+      let e = unwrap entry
+      calculatedRewards <-
+        calculateRewards
+          interest
+          e.deposited
+      assocListTn <-
+        liftContractM
+          "mkEntryUpdateList: Could not create token name for user"
+          $ mkTokenName e.key
+      -- Update the entry datum
+      let
+        recentRewards = roundUp calculatedRewards
+        updatedDeposited = e.deposited + recentRewards
+        newRewards = e.rewards + mkRatUnsafe (recentRewards % one)
+        -- Datum and redeemer creation
+        entryDatum = Datum $ toData $ EntryDatum
+          { entry: Entry $ e
+              { newDeposit = zero -- reset to zero
+              , deposited = updatedDeposited
+              , staked = updatedDeposited -- redundant
+              , rewards = newRewards
+              }
+          }
+        valRedeemer = Redeemer $ toData AdminAct
+        -- Build asset datum and value types
+        assetDatum = Datum $ toData AssetDatum
+        assetParams = unwrap bondedAssetClass
+        assetCs = assetParams.currencySymbol
+        assetTn = assetParams.tokenName
+        depositValue = singleton assetCs assetTn recentRewards
+        entryValue = singleton assocListCs assocListTn one
+
+        -- Build constraints and lookups
+        constraints :: TxConstraints Unit Unit
+        constraints =
+          mconcat
+            [ mustPayToScript valHash assetDatum depositValue
+            , mustPayToScript valHash entryDatum entryValue
+            , mustSpendScriptOutput txIn valRedeemer
+            ]
+      entryDatumLookup <-
+        liftContractM
+          "mkEntryUpdateList: Could not create state datum lookup"
+          $ ScriptLookups.datum entryDatum
+      pure (constraints /\ entryDatumLookup)
+    _ -> throwContractError
+      "mkEntryUpdateList: Datum not Entry constructor"
+
+-- | Calculates user rewards
+calculateRewards
+  :: Rational -- interest
+  -> BigInt -- deposited
+  -> Contract () Rational
+calculateRewards interest deposited = do
+  when (deposited == zero) $
+    throwContractError "calculateRewards: totalDeposited is zero"
+  let
+    recentRewards = interest * mkRatUnsafe (deposited % one)
+  when (recentRewards < zero) $ throwContractError
+    "calculateRewards: invalid rewards amount"
+  pure recentRewards
