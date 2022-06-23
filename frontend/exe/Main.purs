2c958a7aab360b64bf7fcbae502272aa7f749f50
rmgaray <ramiro.sgaray@gmail.com>
2022-06-23 12:13:56 -0300

    increase user stake value in test

--- frontend/exe/Main.purs
+++ frontend/exe/Main.purs
@@ -90,7 +90,7 @@ main = launchAff_ do
   ---- User 1 deposits ----
   userCfg <- mkConfig
   userStake <- liftM (error "main: Cannot create userStake from String") $
-    Natural.fromString "10"
+    Natural.fromString "4000"
   runContract_ userCfg do
     userStakeBondedPoolContract bondedParams userStake
     logInfo' "SWITCH WALLETS NOW - CHANGE TO BACK TO ADMIN"
