686a0809e0f9cb1aaff855e0a1f9cd8a19bb9651
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-08-03 12:20:45 +0700

    Finish the TS example

--- frontend/ts-example/index.ts
+++ frontend/ts-example/index.ts
@@ -1,3 +1,6 @@
+// This module demonstrates how to use the `singularitynet` TS/JS SDK to
+// operate a bonded pool across the entire application lifecycle
+
 import {
   SdkConfig,
   BondedPool,
@@ -6,9 +9,21 @@ import {
 } from "singularitynet";
 
 const singularitynet = require("singularitynet");
-const bigInt = require("big-integer");
+// We need to use `big-integer` directly instead of the `BigInt` constructor
+// as this package defines some methods that are required by the PS code
+const BigInteger = require("big-integer");
 
+// Runs all of the operations defined for a `BondedPool` and simulates the
+// entire pool lifecycle
+//
+// Make sure to switch wallets as directed
 const main = async () => {
+  // some helpers for logging directions to switch wallets
+  const admin = "ADMIN";
+  const user = "USER";
+
+  // Admin creates pool
+  console.log(`STARTING AS ${admin}`);
   const date = new Date();
   console.log(
     `Bonded pool creation: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
@@ -17,27 +32,53 @@ const main = async () => {
     localSdkConfig,
     initialBondedArgs
   );
-
   const bondedPoolArgs: BondedPoolArgs = bondedPool.args;
+  await logSwitchAndCountdown(user, "pool start", bondedPoolArgs.start);
 
-  console.log("SWITCH WALLETS NOW - CHANGE TO USER 1");
-  console.log("Waiting for pool start...");
-  await countdownTo(Number(bondedPoolArgs.start));
+  // User stakes, waiting for pool start
+  const userStakeAmt = BigInteger(40000);
+  await bondedPool.userStake(userStakeAmt);
+  await logSwitchAndCountdown(
+    admin,
+    "bonding period",
+    bondedPoolArgs.start + bondedPoolArgs.userLength
+  );
+
+  // Admin deposits to pool
+  const depositBatchSize = BigInteger(1);
+  await bondedPool.deposit(depositBatchSize, []);
+  await logSwitchAndCountdown(
+    user,
+    "withdrawing  period",
+    bondedPoolArgs.start +
+      bondedPoolArgs.userLength +
+      bondedPoolArgs.bondingLength
+  );
+
+  // User withdraws
+  await bondedPool.userWithdraw();
+  await logSwitchAndCountdown(admin, "closing period", bondedPoolArgs.end);
+
+  // Admin closes pool
+  const closeBatchSize = BigInteger(10);
+  await bondedPool.close(closeBatchSize, []);
+
+  console.log("Pool closed");
 };
 
 const initialBondedArgs: InitialBondedArgs = {
-  iterations: bigInt(2),
-  start: bigInt(1000),
-  end: bigInt(2000),
-  userLength: bigInt(180000),
-  bondingLength: bigInt(180000),
-  interest: [bigInt(10), bigInt(100)],
-  minStake: bigInt(1),
-  maxStake: bigInt(50000),
-  bondedAssetClass: [
-    "6f1a1f0c7ccf632cc9ff4b79687ed13ffe5b624cce288b364ebdce50",
-    "AGIX",
-  ],
+  iterations: BigInteger(2),
+  start: BigInteger(1000),
+  end: BigInteger(2000),
+  userLength: BigInteger(180000),
+  bondingLength: BigInteger(180000),
+  interest: { numerator: BigInteger(10), denominator: BigInteger(100) },
+  minStake: BigInteger(1),
+  maxStake: BigInteger(50000),
+  bondedAssetClass: {
+    currencySymbol: "6f1a1f0c7ccf632cc9ff4b79687ed13ffe5b624cce288b364ebdce50",
+    tokenName: "AGIX",
+  },
 };
 
 const localSdkConfig: SdkConfig = {
@@ -63,6 +104,18 @@ const localSdkConfig: SdkConfig = {
   logLevel: "Info",
 };
 
+// Helpers
+
+const logSwitchAndCountdown = async (
+  who: "USER" | "ADMIN", // the wallet to switch to
+  what: string, // what we're waiting for
+  time: BigInteger // how long to wait
+) => {
+  console.log(`SWITCH WALLETS NOW - CHANGE TO ${who}`);
+  console.log("Waiting for ${what}...");
+  await countdownTo(Number(time));
+};
+
 const countdownTo = async (tf: number) => {
   const now = Date.now();
   if (now > tf) {
@@ -77,6 +130,7 @@ const sleep = async (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 const showSecondsDiff = (x: number, y: number) => (x - y) / 1000;
 
+// Run the contracts, see `main` above
 main()
   .then((_) => {})
   .catch((e) => console.log(e));
