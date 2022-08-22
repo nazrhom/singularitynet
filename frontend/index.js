445577d8499d16fd03ffc6641bda22c8a76d1015
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-08-22 15:52:45 +0700

    Use `exports` directly

--- frontend/index.js
+++ frontend/index.js
@@ -2,27 +2,7 @@
 
 const frontend = import("./output.js");
 
-exports.BondedPool = BondedPool;
-
-exports.UnbondedPool = UnbondedPool;
-
-exports.createBondedPool = async (sdkConfig, initialArgs) => {
-  const contracts = await frontend;
-  const config = await contracts.buildContractConfig(sdkConfig)();
-  const bondedArgs = await contracts.callCreateBondedPool(config)(
-    initialArgs
-  )();
-  return new BondedPool(config, bondedArgs);
-};
-
-exports.createUnbondedPool = async (sdkConfig, initialArgs) => {
-  const contracts = await frontend;
-  const config = await contracts.buildContractConfig(sdkConfig)();
-  const unbondedArgs = await callCreateUnbondedPool(config)(initialArgs)();
-  return new UnbondedPool(config, unbondedArgs);
-};
-
-class BondedPool {
+exports.BondedPool = class BondedPool {
   constructor(config, args) {
     this.config = config;
     this.args = args;
@@ -51,9 +31,9 @@ class BondedPool {
     const contracts = await frontend;
     return contracts.callUserWithdrawBondedPool(this.config)(this.args)();
   }
-}
+};
 
-class UnbondedPool {
+exports.UnbondedPool = class UnbondedPool {
   constructor(config, args) {
     this.config = config;
     this.args = args;
@@ -84,4 +64,20 @@ class UnbondedPool {
     const contracts = await frontend;
     return contracts.callUserWithdrawUnbondedPool(this.config)(this.args)();
   }
-}
+};
+
+exports.createBondedPool = async (sdkConfig, initialArgs) => {
+  const contracts = await frontend;
+  const config = await contracts.buildContractConfig(sdkConfig)();
+  const bondedArgs = await contracts.callCreateBondedPool(config)(
+    initialArgs
+  )();
+  return new exports.BondedPool(config, bondedArgs);
+};
+
+exports.createUnbondedPool = async (sdkConfig, initialArgs) => {
+  const contracts = await frontend;
+  const config = await contracts.buildContractConfig(sdkConfig)();
+  const unbondedArgs = await callCreateUnbondedPool(config)(initialArgs)();
+  return new exports.UnbondedPool(config, unbondedArgs);
+};
